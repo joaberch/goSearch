332ff9bbd1f9328f19f63d7e759c06102b3ea594
joaberch <122774888+joaberch@users.noreply.github.com>
2025-06-23 16:20:47 +0200

    chore: plan the application

--- main.go
+++ main.go
@@ -8,10 +8,27 @@ import (
 )
 
 func main() {
+	//Step 1 - Get Files
+	//Step 1.1 - UI Folder selection
 	selected := selectFolder()
 	tree := treebuilder.GetFileTree(selected)
 	fmt.Println(tree)
 	printTree(tree, 0)
+
+	//Step 2 - Read file in streaming and normalize content
+	//Step 2.1 - Filter (exe, etc)
+
+	//Step 2.2 - Stream file content
+
+	//Step 2.3 - Normalize text content
+
+	//Step 2.4 - Store normalized content (temporarily)
+
+	//Step 3 - Create Inverted index
+	//Step 3.1 - Create XML (Be wary of depth, filter extension like exe to save resources, prevent infinite loop because of symbolic link and display the number of file to index)
+
+	//Step 4 - Save Index
+	//Step 4.1 - Compress XML to store
 }
 
 // Debug func
