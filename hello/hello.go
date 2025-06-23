1eb1d1f28a21bfe7a7069c05fd28deb52177bb0e
joaberch <122774888+joaberch@users.noreply.github.com>
2025-06-23 13:13:23 +0200

    chore: create base's project

---
+++ hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Get a greeting message and print it.
+	message, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(message)
+}
