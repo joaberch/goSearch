e21cb6d64bcb0732c59dff66df204a31a4604046
joaberch <122774888+joaberch@users.noreply.github.com>
2025-06-23 15:42:52 +0200

    feat: select file/folder to get

--- main.go
+++ main.go
@@ -1,22 +1,100 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fmt"
+	"github.com/ncruces/zenity"
+	"log"
+	"os"
 )
 
+type TreeElement struct {
+	Name     string
+	Path     string
+	IsDir    bool
+	Children []TreeElement
+}
+
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
+	selected := selectF()
+	tree := getFileTree(selected)
+	printTree(tree, 0)
+}
 
-	hello := widget.NewLabel("Hello World")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("World")
-		}),
-	))
+func printTree(tree TreeElement, depth int) {
+	indentation := ""
+	for i := 0; i < depth; i++ {
+		indentation += " "
+	}
 
-	w.ShowAndRun()
+	if tree.IsDir {
+		fmt.Printf("%s📁 %s\n", indentation, tree.Name)
+	} else {
+		fmt.Printf("%s📄 %s\n", indentation, tree.Name)
+	}
+
+	for _, child := range tree.Children {
+		printTree(child, depth+1)
+	}
+}
+
+func selectF() []string {
+	file, err := zenity.SelectFileMultiple(zenity.Directory())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Emplacement : ", file)
+	return file
+}
+
+func getFileTree(selected []string) TreeElement {
+	treeRoot := TreeElement{
+		Name:     "root",
+		Path:     "",
+		IsDir:    true,
+		Children: []TreeElement{},
+	}
+
+	for _, element := range selected {
+		info, err := os.Stat(element)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		treeElement := TreeElement{
+			Name:  info.Name(),
+			Path:  element,
+			IsDir: info.IsDir(),
+		}
+		if info.IsDir() {
+			treeElement.Children = recursiveTree(element)
+		}
+
+		treeRoot.Children = append(treeRoot.Children, treeElement)
+	}
+	return treeRoot
+}
+
+func recursiveTree(path string) []TreeElement {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var children []TreeElement
+
+	for _, entry := range entries {
+		childPath := path + "\\" + entry.Name()
+
+		child := TreeElement{
+			Name:  entry.Name(),
+			Path:  childPath,
+			IsDir: entry.IsDir(),
+		}
+
+		if entry.IsDir() {
+			child.Children = recursiveTree(childPath)
+		}
+		children = append(children, child)
+	}
+	return children
 }
