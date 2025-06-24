1f97e8b0e038a855876e7fe3bc15522b96011b05
joaberch <122774888+joaberch@users.noreply.github.com>
2025-06-24 09:09:11 +0200

    feat: create the inverted index

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ type dataFile struct {
 	token []string
 }
 
+type InvertedIndex map[string][]string
+
 func main() {
 	//Step 1 - Get Files
 	//Step 1.1 - UI Folder selection
@@ -25,15 +27,31 @@ func main() {
 	//Step 2.1 - Stream and normalize file content
 	var filesData []dataFile
 	for _, file := range res.ValidFiles {
-		toName := streamer.Stream(file)
+		toName := streamer.Stream(file)                                    //Shortcut are seen as file and not as folder, prevent stackOverflow error
 		filesData = append(filesData, dataFile{path: file, token: toName}) //Step 2.2 - Store normalized content (temporarily)
 	}
 
 	//Step 3 - Create Inverted index
-	//Step 3.1 - Create XML (Be wary of depth, filter extension like exe to save resources, prevent infinite loop because of symbolic link and display the number of file to index)
+	invertedIndex := buildInvertedIndex(filesData)
+	fmt.Println(invertedIndex)
+	//Step 3.1 - Create XML (Be wary of depth, prevent infinite loop because of symbolic link and display the number of file to index)
 
 	//Step 4 - Save Index
 	//Step 4.1 - Compress XML to store
+}
+
+func buildInvertedIndex(files []dataFile) InvertedIndex {
+	index := make(InvertedIndex) //Create an object
+	for _, file := range files { //Foreach file
+		seen := make(map[string]bool)      //Create a list of word added to the file
+		for _, token := range file.token { //Foreach word in the file
+			if !seen[token] { //If the word doesn't already have the file path - could be removed to add a system of weight
+				index[token] = append(index[token], file.path) //Add the file path to the word
+				seen[token] = true                             //Mark the word as processed
+			}
+		}
+	}
+	return index
 }
 
 // Debug func
