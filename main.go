b3c3e5b7cb6a3d3842eed5000bd96e523e13adc4
joaberch <122774888+joaberch@users.noreply.github.com>
2025-06-24 10:50:22 +0200

    feat: compress XML file

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"Go-LocalSearchEngine/streamer"
 	"Go-LocalSearchEngine/treebuilder"
 	"Go-LocalSearchEngine/uier"
+	"bufio"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"log"
+	"os"
 )
 
 func main() {
@@ -36,4 +40,49 @@ func main() {
 
 	//Step 4 - Save Index
 	//Step 4.1 - Compress XML to store
+	compressXMLFile()
+}
+
+func compressXMLFile() {
+	fileSource := "index.xml"
+	fileDestination := "index.xml.gz"
+
+	//Open source file
+	source, err := os.Open(fileSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(source)
+
+	//Create destination file
+	destination, err := os.Create(fileDestination)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(destination)
+
+	//Create writer
+	w := gzip.NewWriter(destination)
+	defer func(w *gzip.Writer) {
+		err := w.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(w)
+
+	//Copy content
+	_, err = io.Copy(w, bufio.NewReader(source))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
