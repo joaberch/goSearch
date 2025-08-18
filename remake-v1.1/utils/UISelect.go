fdcc803e271bc0d67fd8415692b7ef38c1d832cf
joaberch <122774888+joaberch@users.noreply.github.com>
2025-08-18 16:46:11 +0200

    chore: add UI title

--- remake-v1.1/utils/UISelect.go
+++ remake-v1.1/utils/UISelect.go
@@ -5,7 +5,7 @@ import "github.com/ncruces/zenity"
 // UISelectFolder opens a directory picker and returns the selected path.
 // It returns a non-nil error if the user cancels the dialog or if an error occurs.
 func UISelectFolder() (string, error) {
-	file, err := zenity.SelectFile(zenity.Directory())
+	file, err := zenity.SelectFile(zenity.Directory(), zenity.Title("Select a folder"))
 	if err != nil {
 		return "", err
 	}
