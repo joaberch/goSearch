480f997e8244850a3aad588cbf0b1bfee23a35bf
joaberch <122774888+joaberch@users.noreply.github.com>
2025-08-18 15:10:57 +0200

    feat: search help

--- remake-v1.1/cmd/help.go
+++ remake-v1.1/cmd/help.go
@@ -1,5 +1,13 @@
 package cmd
 
+import "fmt"
+
 func ShowHelp() {
-	//TODO
+	fmt.Printf("Usage:\n\tcmd <command> [<args>]\n\n")
+	fmt.Printf("The commands are:\n")
+	fmt.Printf("\tversion\tdisplay the version")
+	fmt.Printf("\thelp\tdisplay this help")
+	fmt.Printf("\tsearch [arg]\tsearch the argument in the index")
+	fmt.Printf("\tindexate\tcreate an index from the path given")
+	fmt.Printf("\tdisplay\tdisplay the structure of the file indexed")
 }
