60694228db80cfb8fe674f693f35d91e93c3cfe5
joaberch <122774888+joaberch@users.noreply.github.com>
2025-08-21 16:58:26 +0200

    feat(indexate): compress file by giving path and name

--- remake-v1.1/utils/CompressFile.go
+++ remake-v1.1/utils/CompressFile.go
@@ -1,1 +1,50 @@
 package utils
+
+import (
+	"bufio"
+	"compress/gzip"
+	"io"
+	"os"
+)
+
+// CompressFile compress the file given with its path and its name
+func CompressFile(path string, name string) {
+	//Step 1 - Open source file
+	source, err := os.Open(path + "\\" + name)
+	if err != nil {
+		panic(err)
+	}
+	defer func(source *os.File) {
+		err := source.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(source)
+
+	//Step 2 - Create output file
+	dest, err := os.Create(path + ".gz")
+	if err != nil {
+		panic(err)
+	}
+	defer func(f *os.File) {
+		err := dest.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(dest)
+
+	//Step 3 - Create writer
+	writer := gzip.NewWriter(dest)
+	defer func(writer *gzip.Writer) {
+		err := writer.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(writer)
+
+	//Step 4 - Copy source to dest
+	_, err = io.Copy(writer, bufio.NewReader(source))
+	if err != nil {
+		panic(err)
+	}
+}
