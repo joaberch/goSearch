4b2353a1349c00b60a6bd5aa87b677c8aff3e4f2
joaberch <122774888+joaberch@users.noreply.github.com>
2025-08-29 08:20:10 +0200

    refactor: use flag

--- goSearch/main.go
+++ goSearch/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"goSearch/cmd"
 	"os"
 )
 
 func main() {
-	userCommand, err := cmd.GetCommandType(os.Args[1])
-	if err != nil {
-		fmt.Println(err)
+	versionFlag := flag.Bool("v", false, "print version string")
+	longVersionFlag := flag.Bool("version", false, "print version string")
+	helpFlag := flag.Bool("h", false, "print usage string")
+	longHelpFlag := flag.Bool("help", false, "print usage string")
+	flag.Parse()
+
+	args := os.Args[1:]
+
+	if *versionFlag || *longVersionFlag {
+		cmd.ShowVersion()
+		return
+	}
+
+	if *helpFlag || *longHelpFlag || len(args) == 0 {
 		cmd.ShowHelp()
 		return
 	}
 
-	switch userCommand {
-	case cmd.CmdShowVersion:
-		cmd.ShowVersion()
-		break
-	case cmd.CmdSearch:
-		word := os.Args[1]
-		cmd.Search(word)
-		break
-	default:
-		cmd.ShowHelp()
-	}
+	word := args[0]
+	cmd.Search(word)
 }
