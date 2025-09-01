1b828914dc3afab1953ede6a8f9d97018521f67e
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-01 15:47:18 +0200

    refactor: use same naming convention

---
+++ goSearch/utils/display_result.go
@@ -0,0 +1,11 @@
+package utils
+
+import "fmt"
+
+// DisplayResult prints the list of file paths where the word was found.
+func DisplayResult(results map[string]bool, word string) { //FUTURE: Filter/Sort result
+	fmt.Printf("\nFound %d file(s) for \"%s\":\n", len(results), word)
+	for path := range results {
+		fmt.Printf("\t%s\n", path)
+	}
+}
