3baadcc62330587d27d859355d8f15c2f1360507
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-01 13:21:47 +0200

    chore(help): refactor help

--- goSearch/cmd/help.go
+++ goSearch/cmd/help.go
@@ -3,9 +3,26 @@ package cmd
 import "fmt"
 
 func ShowHelp() {
-	fmt.Printf("Usage:\n\tcmd <command> [<args>]\n\n")
-	fmt.Printf("The commands are:\n")
-	fmt.Printf("\tversion\t\tdisplay the version\n")
-	fmt.Printf("\thelp\t\tdisplay this help\n")
-	fmt.Printf("\t<word>\t\tsearch the word in the index generated from current position\n")
+	fmt.Println("Usage:")
+	fmt.Println("  gosearch [options] <word>             Search for a word in the current directory")
+	fmt.Println("  gosearch [options] -use <word> <name> Search for a word using a saved index")
+	fmt.Println("  gosearch -save [path]                 Save an index of the current or specified path")
+	fmt.Println("  gosearch -version                     Display version information")
+	fmt.Println("  gosearch -help                        Display this help message")
+	fmt.Println()
+
+	fmt.Println("Options:")
+	fmt.Println("  -v, --v, -version, --version          Show version information")
+	fmt.Println("  -h, --h, -help, --help                Show usage help")
+	fmt.Println("  -s, --s, -save, --save                Save index of current or specified path in Desktop/utils/index")
+	fmt.Println("  -u, --u, -use, --use                  Use a specific saved index for search")
+	fmt.Println()
+
+	fmt.Println("Examples:")
+	fmt.Println("  gosearch hello                        Search for 'hello' in current directory")
+	fmt.Println("  gosearch -s                           Save index of current directory")
+	fmt.Println("  gosearch -s /path/to/folder           Save index of specified folder")
+	fmt.Println("  gosearch -u hello temp                Search for 'hello' in index named 'temp'")
+	fmt.Println("  gosearch -version                     Show version info")
+	fmt.Println("  gosearch -help                        Show this help message")
 }
