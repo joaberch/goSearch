05e0e8176b90375167c9f66ec0a7a4b51250fe70
joaberch <122774888+joaberch@users.noreply.github.com>
2025-09-04 16:02:02 +0200

    refactor(indexing): create the index with the word in alphabetical order

--- utils/convert_inverted_index_to_xml.go
+++ utils/convert_inverted_index_to_xml.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/joaberch/goSearch/internal/model"
+import (
+	"github.com/joaberch/goSearch/internal/model"
+	"sort"
+)
 
 // ConvertInvertedIndexToXML transforms an InvertedIndex into an IndexDocument for XML serialization.
 func ConvertInvertedIndexToXML(index model.InvertedIndex) model.IndexDocument { //Future: Sort by word for manual research and/or diff git
@@ -8,5 +11,10 @@ func ConvertInvertedIndexToXML(index model.InvertedIndex) model.IndexDocument { 
 	for word, files := range index {
 		entries = append(entries, model.IndexEntry{Word: word, Files: files})
 	}
+
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].Word < entries[j].Word
+	})
+
 	return model.IndexDocument{Entries: entries}
 }
