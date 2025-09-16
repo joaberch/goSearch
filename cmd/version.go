c4d8970fdb1b2da383a333a7fd9a1d12e10ac65a
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2025-09-16 15:21:20 +0200

    chore(version): update version

--- cmd/version.go
+++ cmd/version.go
@@ -3,5 +3,5 @@ package cmd
 import "fmt"
 
 func ShowVersion() {
-	fmt.Printf("Version: %s\nDate: %s\nGo: %s\n", "1.1.0", "   05.09.2025", "     go 1.25.0 windows/amd64")
+	fmt.Printf("Version: %s\nDate: %s\nGo: %s\n", "1.2.0", "   16.09.2025", "     go 1.25.0 windows/amd64")
 }
